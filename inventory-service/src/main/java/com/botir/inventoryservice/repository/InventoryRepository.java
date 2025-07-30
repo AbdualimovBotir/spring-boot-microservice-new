de94207fe7fcc1591f29db0537d36bbbd9c34f31
AbdualimovBotir <botirabdualimov07@gmail.com>
2025-07-30 12:25:32 +0500

    feat: add full microservices-based e-commerce architecture with API Gateway, Eureka, Kafka, and observability stack

---
+++ inventory-service/src/main/java/com/botir/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package com.botir.inventoryservice.repository;
+
+import com.botir.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
+}
---
+++ product-service/src/main/java/com/botir/productservice/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.botir.productservice.repository;
+
+import com.botir.productservice.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
